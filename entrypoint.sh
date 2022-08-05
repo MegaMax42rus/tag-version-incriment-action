7835090403b4bf4c70bcf81f4887d1d0b300e106
Maxim Volshin <megamax42rus@gmail.com>
2022-08-05 12:57:54 +0700

    Fix: disable debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 git config --global --add safe.directory /github/workspace
-git tag
-git describe --tags
 if [ -z $(git describe --tags | grep -Po '^(v\d*.\d*.)(\d*)-\d*-.*' | cut -d. -f3 | cut -d- -f1) ];
 	then
 		echo "::set-output name=docker_tag::$REGISTRY/$IMAGE_NAME:$(git describe --tags)" | tr '[:upper:]' '[:lower:]'
